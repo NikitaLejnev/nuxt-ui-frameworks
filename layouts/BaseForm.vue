30e629dddc87aabd9278d6e4dc166842dafae20b
Nikita Lezhnev <niklezhnev44@gmail.com>
2022-03-29 11:23:43 +0300

    Configure Foundation

---
+++ layouts/BaseForm.vue
@@ -0,0 +1,9 @@
+<script>
+import $ from 'jquery'
+
+export default {
+  mounted() {
+    $(document).foundation()
+  },
+}
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -15,10 +15,10 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['foundation-sites/dist/css/foundation.min.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['~/plugins/client-only/foundation.client.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
